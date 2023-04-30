6e09eb659d2885abd324f9d063917ee3e63862f9
Saturn1B <60229050+Saturn1B@users.noreply.github.com>
2023-04-30 23:11:21 +0200

    setup all

--- Assets/01_Scripts/ContractManager.cs
+++ Assets/01_Scripts/ContractManager.cs
@@ -13,10 +13,30 @@ public class ContractManager : MonoBehaviour
     [SerializeField]
     GameObject boxTextPrefab;
 
+    private void Awake()
+    {
+        if (Screen.width != 1920)
+        {
+            GridLayoutGroup grid = GetComponent<GridLayoutGroup>();
+
+            Debug.Log(Screen.width);
+            int xCoor = Mathf.FloorToInt(Screen.width * 0.15625f);
+            int yCoor = Mathf.FloorToInt(xCoor * 1.3f);
+            grid.cellSize = new Vector2(xCoor, yCoor);
+            grid.enabled = false;
+            grid.enabled = true;
+
+            int yCoor2 = Mathf.FloorToInt(xCoor * .216f);
+            transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(xCoor, yCoor2);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        foreach(ContractScriptable contract in contracts)
+
+
+        foreach (ContractScriptable contract in contracts)
         {
             GameObject go = Instantiate(contractPrefab);
             go.transform.SetParent(transform);
@@ -28,7 +48,15 @@ public class ContractManager : MonoBehaviour
                 go.transform.GetChild(4).gameObject.SetActive(false);
                 go.GetComponent<Button>().interactable = true;
             }
-            foreach(BoxType box in contract.boxTypes)
+
+            if (Screen.width != 1920)
+            {
+                float spacing = Screen.width * 0.0015f;
+
+                go.transform.GetChild(2).GetComponent<VerticalLayoutGroup>().spacing = spacing;
+            }
+
+            foreach (BoxType box in contract.boxTypes)
             {
                 GameObject go2 = Instantiate(boxTextPrefab);
                 go2.transform.SetParent(go.transform.GetChild(2).transform);
@@ -37,6 +65,14 @@ public class ContractManager : MonoBehaviour
                 {
                     go.GetComponent<ContractButton>().boxToSpawn.Add(box.boxPrefab);
                     go.GetComponent<ContractButton>().placeToSpawn.Add(box.boxPos[i]);
+                }
+
+                if (Screen.width != 1920)
+                {
+                    int xCoor = Mathf.FloorToInt(Screen.width * 0.145f);
+                    int yCoor = Mathf.FloorToInt(xCoor * 0.20f);
+
+                    go2.GetComponent<RectTransform>().sizeDelta = new Vector2(xCoor, yCoor);
                 }
             }
         }
